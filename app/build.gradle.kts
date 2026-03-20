7a5c1da15128e4583372ed60b8ccefcddb7c3b80
volgunov_ad <volgunov@gmail.com>
2026-03-20 23:09:42 +0300

    Режим запуска: единичная активность

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "vad.dashing.tbox"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1200
-        versionName = "0.12.0"
+        versionCode = 1300
+        versionName = "0.13.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
