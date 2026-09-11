749ea02ce27cb4c42cc683c8868af0181dde785a
volgunov_ad <volgunov@gmail.com>
2026-09-11 14:49:05 +0300

    Bump versionCode to 10002 for 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         applicationId = "vad.dashing.tbox"
         minSdk = 28
         targetSdk = 36
-        versionCode = 10001
+        versionCode = 10002
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
