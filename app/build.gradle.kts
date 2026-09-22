336266ec478c94ac2c661da6a687ed7562f5ebf7
volgunov_ad <volgunov@gmail.com>
2026-09-22 15:18:51 +0300

    1.0.0 10010

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         applicationId = "vad.dashing.tbox"
         minSdk = 28
         targetSdk = 36
-        versionCode = 10009
+        versionCode = 10010
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
